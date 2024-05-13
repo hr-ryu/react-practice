02d1c1ca0cf618d6cde33b4cef617338557ce77e
hr-ryu <tomato.202404.hayakawarnsk@gmail.com>
2024-05-13 17:33:01 +0900

    1702

--- src/conponents/card/card.js
+++ src/conponents/card/card.js
@@ -5,6 +5,7 @@ import { Container, NameBox, CheckBox } from "./styled";
 
 export const Card = ({name,number}) => {
     const [checkNum, setCheckNum] = useState(0);
+    if(checkNum < 0){ setCheckNum(0); }
    
     return (
         <Container>
@@ -12,8 +13,8 @@ export const Card = ({name,number}) => {
                 <p> {number} : {name} </p>
             </NameBox>
             <CheckBox>
-                <button onClick={() => setCheckNum(checkNum + 1)} > + 1 </button>
-                <button onClick={() => setCheckNum(checkNum - 1)} > - 1 </button>
+                <button onClick={() => setCheckNum(checkNum + 1)}> + 1 </button>
+                <button onClick={() => setCheckNum(checkNum - 1)}> - 1 </button>
                 <span>  {checkNum}</span>
             </CheckBox>
         </Container>
